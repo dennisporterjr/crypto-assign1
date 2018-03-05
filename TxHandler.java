c9c3465d0cbc1e2666c923362e6b3b9b6874ddb6
Dennis Porter <dennisporter@quickenloans.com>
2018-03-05 18:20:02 -0500

    first pass at a usualable TxHandler.isValidTx method.

--- TxHandler.java
+++ TxHandler.java
@@ -33,15 +33,15 @@ public class TxHandler {
     /**
      * @return true if:
      * (3) no UTXO is claimed multiple times by {@code tx},
-     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // Transaction inputs and outputs
-        ArrayList<Transaction.Input> inputs = tx.getInputs();
-        ArrayList<Transaction.Output> outputs = tx.getOutputs();
 
-        // Output/Input map
+        ArrayList<Transaction.Input> inputs = tx.getInputs();
+        int inputSize = inputs.size();
+
+        ArrayList<Transaction.Output> outputs = tx.getOutputs();
+        int outputSize = outputs.size();
+
         HashMap<Integer,byte[]> opHashes;
         opHashes = new HashMap<Integer, byte[]>();
 
@@ -49,46 +49,45 @@ public class TxHandler {
         double totalInput = 0.0;
         double totalOutput = 0.0;
 
-        int inputSize = inputs.size();
         for (int i = 0; i < inputSize; i ++) {
-            System.out.println("\ninput " + i);
-            System.out.println("---------------------------------");
+
             UTXO unspent = new UTXO(inputs.get(i).prevTxHash, inputs.get(i).outputIndex);
             Transaction.Output ut = this.utxoPool.getTxOutput(unspent);
 
             // (2) the signatures on each input of {@code tx} are valid,
-            System.out.println("verified: " +
-                    Crypto.verifySignature(ut.address, tx.getRawDataToSign(inputs.get(i).outputIndex), inputs.get(i).signature));
+            if (!Crypto.verifySignature(ut.address, tx.getRawDataToSign(inputs.get(i).outputIndex), inputs.get(i).signature)) {
+                return false;
+            }
 
             totalInput += ut.value;
-
             opHashes.put(inputs.get(i).outputIndex, inputs.get(i).prevTxHash);
         }
 
-        int outputSize = outputs.size();
         for (int i = 0; i < outputSize; i ++) {
-            System.out.println("\noutput " + i);
-            System.out.println("---------------------------------");
 
             // (4) all of {@code tx}s output values are non-negative, and
-            System.out.println("output value non-negative: " + (outputs.get(i).value > 0));
-
-            totalOutput += outputs.get(i).value;
+            if (outputs.get(i).value <= 0) {
+                return false;
+            }
 
             // (1) all outputs claimed by {@code tx} are in the current UTXO pool,
             if (opHashes.containsKey(i)) {
-                System.out.println("in current pool: " + this.utxoPool.contains(new UTXO(opHashes.get(i), i)));
+                if (!this.utxoPool.contains(new UTXO(opHashes.get(i), i))) {
+                    return false;
+                }
             } else {
-                System.out.println("in current pool: false");
+                return false;
             }
+
+            totalOutput += outputs.get(i).value;
         }
 
-        System.out.println("\n\ni/o equal: " + (totalInput >= totalOutput));
-        System.out.println("total input: " + (totalInput));
-        System.out.println("total output: " + (totalOutput));
+        // (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        // values; and false otherwise.
+        if (totalInput < totalOutput) {
+            return false;
+        }
 
-
-        System.out.println("\n\n");
         return true;
     }
 
